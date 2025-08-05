0817fa542b77f139c5b0a7483b8c98e785b30257
rub-a-dub-dub <pradip@thachile.com>
2025-08-04 20:52:10 -0700

    Revert "Require Slack user token (#24)"
    This reverts commit 7611e744f2be8fa41d38ffb8da940282e5310705.

--- config/config_test.go
+++ config/config_test.go
@@ -1,8 +1,7 @@
-package config_test
+package config
 
 import (
 	"github.com/stretchr/testify/assert"
-	"heckel.io/replbot/config"
 	"os"
 	"path/filepath"
 	"testing"
@@ -18,12 +17,11 @@ func TestNew(t *testing.T) {
 	if err := os.WriteFile(script2, []byte{}, 0700); err != nil {
 		t.Fatal(err)
 	}
-	conf := config.New("xoxb-slack-token", "xapp-slack-app-token")
-	conf.UserToken = "xoxp-slack-user-token"
+	conf := New("xoxb-slack-token", "xapp-slack-app-token")
 	conf.ScriptDir = dir
 	platform, err := conf.Platform()
 	assert.NoError(t, err)
-	assert.Equal(t, config.Slack, platform)
+	assert.Equal(t, Slack, platform)
 	assert.ElementsMatch(t, []string{"script1", "script2"}, conf.Scripts())
 	assert.False(t, conf.ShareEnabled())
 	assert.Empty(t, conf.Script("does-not-exist"))
@@ -32,35 +30,12 @@ func TestNew(t *testing.T) {
 }
 
 func TestNewDiscordShareHost(t *testing.T) {
-	conf := config.New("not-slack", "")
+	conf := New("not-slack", "")
 	conf.ShareHost = "localhost:2586"
 	conf.ScriptDir = "/does-not-exist"
 	platform, err := conf.Platform()
 	assert.NoError(t, err)
-	assert.Equal(t, config.Discord, platform)
+	assert.Equal(t, Discord, platform)
 	assert.Empty(t, conf.Scripts())
 	assert.True(t, conf.ShareEnabled())
-}
-
-func TestSlackUserTokenRequired(t *testing.T) {
-	conf := config.New("xoxb-slack-token", "xapp-slack-app-token")
-	_, err := conf.Platform()
-	assert.Error(t, err)
-	if cfgErr, ok := err.(*config.ConfigError); ok {
-		assert.Equal(t, "INVALID_SLACK_USER_TOKEN", cfgErr.Code)
-	} else {
-		t.Fatalf("expected ConfigError, got %T", err)
-	}
-}
-
-func TestSlackInvalidUserToken(t *testing.T) {
-	conf := config.New("xoxb-slack-token", "xapp-slack-app-token")
-	conf.UserToken = "invalid-token"
-	_, err := conf.Platform()
-	assert.Error(t, err)
-	if cfgErr, ok := err.(*config.ConfigError); ok {
-		assert.Equal(t, "INVALID_SLACK_USER_TOKEN", cfgErr.Code)
-	} else {
-		t.Fatalf("expected ConfigError, got %T", err)
-	}
 }
